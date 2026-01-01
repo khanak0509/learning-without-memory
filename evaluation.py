b4beedeb9886cb9392fe45055f457ca50a46097a
khanak0509 <kkhanak512@gmail.com>
2026-01-01 17:09:46 +0530

    update parameters

--- evaluation.py
+++ evaluation.py
@@ -1,6 +1,9 @@
 import re 
 import json
 from numpy import clip 
+from collections import Counter
+
+
 
 with open("parameters.json") as f:
     parameters = json.load(f)
@@ -19,14 +22,37 @@ def count_words(llm_ans):
     words = len(llm_ans.strip().split())
     return words
 
+# def 
+def repetition_penalty(output):
+    words = [w.lower() for w in output.split()]
+    counter = Counter(words)
+    repeated_words = sum([count-1 for count in counter.values() if count > 1])
+    total_words = len(words)
+    return min(1.0, repeated_words / total_words)
+
+
 def main(user_input , llm_ans):
     actual_words = count_words(llm_ans)
     target_words = extract_target_words(user_input)
-    error  = (actual_words - target_words) / target_words
-    parameters["prompt"]["verbosity"] = clip(parameters["prompt"]["verbosity"] - alpha * error, 0.0, 1.0)
-    parameters["decoding"]["temperature"] = clip(parameters["decoding"]["temperature"] - 0.5 * alpha * error, 0.1, 1.2)
-    parameters["weights"]["w_length"] = clip(parameters["weights"]["w_length"] + alpha * abs(error), 0.5, 5.0)
+    length_error  = (actual_words - target_words) / target_words
+    parameters["prompt"]["verbosity"] = clip(parameters["prompt"]["verbosity"] - alpha * length_error, 0.0, 1.0)
+    parameters["decoding"]["temperature"] = clip(parameters["decoding"]["temperature"] - 0.5 * alpha * length_error, 0.1, 1.2)
+    parameters["weights"]["w_length"] = clip(parameters["weights"]["w_length"] + alpha * abs(length_error), 0.5, 5.0)
+
+    repetition_score = repetition_penalty(llm_ans)
+
+    repetition_error = repetition_score - 0.5 
+    parameters["decoding"]["repetition_penalty"] += alpha * repetition_error
+    parameters["decoding"]["repetition_penalty"] = min(max(parameters["decoding"]["repetition_penalty"], 1.0), 2.0)
+    parameters["decoding"]["top_p"] -= alpha * repetition_error
+    parameters["decoding"]["top_p"] = min(max(parameters["decoding"]["top_p"], 0.7), 0.99)
     
+
+
+
+
+
+
 
 
 
