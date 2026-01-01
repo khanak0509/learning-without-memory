e59a1360e77e06a31cc9c68b8e56acae188cc978
khanak0509 <kkhanak512@gmail.com>
2026-01-01 16:25:11 +0530

    add arguments

--- initialize_llm.py
+++ initialize_llm.py
@@ -1,10 +1,15 @@
 from langchain_google_genai import ChatGoogleGenerativeAI
 from dotenv import load_dotenv
 import os
+import json 
+load_dotenv()
 
-load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
-
+with open('parameters.json') as f:
+    parameters = json.load(f)
 
 llm = ChatGoogleGenerativeAI(
-    model = 'gemini-2.5-flash'
+    model = 'gemini-2.5-flash',
+    temperature = parameters['decoding']['temperature'],
+    top_p = parameters['decoding']['top_p'],
+    repetition_penalty=parameters['decoding']['repetition_penalty']
 )
