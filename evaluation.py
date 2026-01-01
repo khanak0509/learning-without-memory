a259cebcf206aeda7bd78d7e7f2c7a6f01e14e28
khanak0509 <kkhanak512@gmail.com>
2026-01-01 16:11:43 +0530

    initalize main function

--- evaluation.py
+++ evaluation.py
@@ -1,5 +1,9 @@
 import re 
+import json 
 
+with open "parameters.json" as f:
+
+alpha = 0.1 
 
 def extract_target_words(user_input):
     match = re.search(r'(\d+)\s*words?', user_input.lower())
@@ -8,3 +12,15 @@ def extract_target_words(user_input):
     return None
 
 # print(extract_target_words("sumarise in 100 word"))
+
+def count_words(llm_ans):
+    words = len(llm_ans.strip().split())
+    return words
+
+def main(user_input , llm_ans):
+    actual_words = count_words(llm_ans)
+    target_words = extract_target_words(user_input)
+    length_error = (actual_words - target_words) / target_words
+
+
+    
