f5820f7780db878d7b6d67f49acd9b440f254409
khanak0509 <kkhanak512@gmail.com>
2026-01-01 14:49:31 +0530

    initialize global llm

---
+++ initialize_llm.py
@@ -0,0 +1,10 @@
+from langchain_google_genai import ChatGoogleGenerativeAI
+from dotenv import load_dotenv
+import os
+
+load_dotenv(os.path.join(os.path.dirname(__file__), '.env'))
+
+
+llm = ChatGoogleGenerativeAI(
+    model = 'gemini-2.5-flash'
+)
